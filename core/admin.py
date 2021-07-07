1bee90d9f0d2d8b6aef53e6fab4c97aa7a67b3c6
Middleway13 <middlewayonlinemedia@gmail.com>
2021-07-07 15:51:09 -0400

    models and admin modified

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
+from core.models import Habit
 from django.contrib import admin
+from .models import User, Habit, HabitRecord, UniqueConstraint
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Habit)
+admin.site.register(HabitRecord)
+admin.site.register(UniqueConstraint)
--- core/models.py
+++ core/models.py
@@ -2,4 +2,30 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    username = models.CharField(max_length=256)
+    email = models.CharField(max_length=256)
+    password = models.CharField(max_length=256)
+    
+    def __str__(self):
+        return self.username
+    
+class Habit(models.Model):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=256)
+    target = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class HabitRecord(models.Model):
+    habit = models.ForeignKey(Habit, null=True, on_delete=models.CASCADE, related_name="records")
+    outcome = models.PositiveIntegerField()
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+
+class UniqueConstraint(fields=['user', 'name', 'date'], name='unique_records')
